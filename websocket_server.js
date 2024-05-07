36c842a7f65b2d329513219cf2e099e7f78efd00
Mickman <70730433+Mickman1@users.noreply.github.com>
2024-05-07 06:08:39 -0400

    profanity filter, chalk for colors

--- websocket_server.js
+++ websocket_server.js
@@ -1,10 +1,20 @@
+const chalk = require('chalk')
+
+// Profanity filter
+const { Profanity, ProfanityOptions } = require('@2toad/profanity')
+
+const options = new ProfanityOptions()
+options.wholeWord = false
+
+const profanity = new Profanity(options)
+
 // VRChat OSC
 const { Client, Server } = require('node-osc')
 
 const oscClient = new Client('127.0.0.1', 9000)
 
 var oscServer = new Server(9001, '127.0.0.1', () => {
-	console.log('\x1b[33mOSC Server online\x1b[0m')
+	console.log(chalk.yellow('OSC Server online'))
 })
 
 oscServer.on('message', function (msg) {
@@ -18,26 +28,27 @@ const SocketServer = require('ws').Server
 
 const expressPort = 2096
 const expressServer = express().listen(expressPort, () => {
-	console.log(`Server started at ${expressPort}`)
+	console.log(chalk.yellow(`Server started at ${expressPort}`))
 })
 
 const wss = new SocketServer({ server: expressServer })
 
 wss.on('connection', (ws, request) => {
-	console.log('[Server] Client connected.')
+	console.log(chalk.greenBright('[Server] Client connected.'))
 
 	let clientUptimeDateStart = Date.now()
 
 	ws.on('close', () => {
-		console.log(`[Server] Client disconnected.\nClient uptime was ${(Date.now() - clientUptimeDateStart) / 1000} seconds.`)
+		console.log(chalk.redBright(`[Server] Client disconnected.\nClient uptime was ${(Date.now() - clientUptimeDateStart) / 1000} seconds.`))
 	})
 
 	ws.on('message', (message) => {
 		// Convert incomming Buffer to readable String
 		let messageString = Buffer.from(message).toString()
-		console.log(messageString)
+		console.log(chalk.cyan(`${new Date().toLocaleTimeString()}: `), chalk.grey(`${messageString}`))
 
-		oscClient.send('/chatbox/input', `${messageString}`, true)
+		// Pass "3" as the CensorType, censors all vowels
+		oscClient.send('/chatbox/input', `${profanity.censor(messageString, 3)}`, true)
 
 		// Broadcast to everyone else connected
 		wss.clients.forEach(function each(client) {
