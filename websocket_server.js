0089828d295e9bdf40f6313e068c2128c3c5b749
Mickman <70730433+Mickman1@users.noreply.github.com>
2024-05-07 23:16:27 -0400

    process message object, added input

--- websocket_server.js
+++ websocket_server.js
@@ -2,10 +2,8 @@ const chalk = require('chalk')
 
 // Profanity filter
 const { Profanity, ProfanityOptions } = require('@2toad/profanity')
-
 const options = new ProfanityOptions()
 options.wholeWord = false
-
 const profanity = new Profanity(options)
 
 // VRChat OSC
@@ -40,12 +38,11 @@ wss.on('connection', (ws, request) => {
 	})
 
 	ws.on('message', (message) => {
-		// Convert incomming Buffer to readable String
-		let messageString = Buffer.from(message).toString()
+		const messageString = message.toString()
+		const messageObject = JSON.parse(message)
 		console.log(chalk.cyan(`${new Date().toLocaleTimeString()}:`), chalk.grey(`${messageString}`))
 
-		// Pass "3" as the CensorType, censors all vowels
-		oscClient.send('/chatbox/input', `${profanity.censor(messageString, 3)}`, true)
+		processMessage(messageObject)
 
 		// Broadcast to everyone else connected
 		wss.clients.forEach(function each(client) {
@@ -55,3 +52,72 @@ wss.on('connection', (ws, request) => {
 		})
 	})
 })
+
+function processMessage(message) {
+	switch (message.type) {
+		case 'input':
+			processInput(message.message)
+			break
+		case 'chatbox':
+			processChat(message.message)
+			break
+	}
+}
+
+function processInput(message) {
+	switch (message) {
+		case 'keyDownForward':
+			oscClient.send('/input/MoveForward', true)
+			break
+		case 'keyDownBackward':
+			oscClient.send('/input/MoveBackward', true)
+			break
+		case 'keyDownLeft':
+			oscClient.send('/input/MoveLeft', true)
+			break
+		case 'keyDownRight':
+			oscClient.send('/input/MoveRight', true)
+			break
+		case 'keyDownSprint':
+			oscClient.send('/input/Run', true)
+			break
+		case 'keyDownJump':
+			oscClient.send('/input/Jump', 1)
+			break
+		case 'keyDownCrouch':
+			oscClient.send('/avatar/parameters/Upright', 0.6)
+			break
+		case 'keyDownProne':
+			oscClient.send('/avatar/parameters/Upright', 0.35)
+			break
+		case 'keyUpForward':
+			oscClient.send('/input/MoveForward', false)
+			break
+		case 'keyUpBackward':
+			oscClient.send('/input/MoveBackward', false)
+			break
+		case 'keyUpLeft':
+			oscClient.send('/input/MoveLeft', false)
+			break
+		case 'keyUpRight':
+			oscClient.send('/input/MoveRight', false)
+			break
+		case 'keyUpSprint':
+			oscClient.send('/input/Run', false)
+			break
+		case 'keyUpJump':
+			oscClient.send('/input/Jump', 0)
+			break
+		case 'keyUpCrouch':
+			oscClient.send('/avatar/parameters/Upright', 1)
+			break
+		case 'keyUpProne':
+			oscClient.send('/avatar/parameters/Upright', 1)
+			break
+	}
+}
+
+function processChat(message) {	
+	// Pass "3" as the CensorType, censors all vowels
+	oscClient.send('/chatbox/input', `${profanity.censor(message, 3)}`, true)
+}
