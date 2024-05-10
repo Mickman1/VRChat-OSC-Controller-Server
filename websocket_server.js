6c0ed53941189300d2c23acba5da0aca61bf054d
Mickman <70730433+Mickman1@users.noreply.github.com>
2024-05-10 00:50:10 -0400

    change inputMap's scope
    - avoid assigning inputMap every processInput() call

--- websocket_server.js
+++ websocket_server.js
@@ -6,14 +6,36 @@ const options = new ProfanityOptions()
 options.wholeWord = false
 const profanity = new Profanity(options)
 
+const inputMap = {
+	'keyDownForward': 	() => oscClient.send('/input/MoveForward', true),
+	'keyDownBackward': 	() => oscClient.send('/input/MoveBackward', true),
+	'keyDownLeft': 			() => oscClient.send('/input/MoveLeft', true),
+	'keyDownRight': 		() => oscClient.send('/input/MoveRight', true),
+	'keyDownSprint': 		() => oscClient.send('/input/Run', true),
+	'keyDownJump': 			() => oscClient.send('/input/Jump', 1),
+	'keyDownCrouch': 		() => oscClient.send('/input/GrabAxisRight', 1),
+	'keyDownProne': 		() => oscClient.send('/avatar/parameters/Upright', 0.35),
+	'keyDownVoice': 		() => oscClient.send('/input/Voice', 1),
+	'keyDownLookLeft': 	() => oscClient.send('/input/LookLeft', 1),
+	'keyDownLookRight': () => oscClient.send('/input/LookRight', 1),
+	'keyUpForward': 		() => oscClient.send('/input/MoveForward', false),
+	'keyUpBackward': 		() => oscClient.send('/input/MoveBackward', false),
+	'keyUpLeft': 				() => oscClient.send('/input/MoveLeft', false),
+	'keyUpRight': 			() => oscClient.send('/input/MoveRight', false),
+	'keyUpSprint': 			() => oscClient.send('/input/Run', false),
+	'keyUpJump': 				() => oscClient.send('/input/Jump', 0),
+	'keyUpCrouch': 			() => oscClient.send('/avatar/parameters/Go/Horizon', false),
+	'keyUpProne': 			() => oscClient.send('/avatar/parameters/Upright', 1),
+	'keyUpVoice': 			() => oscClient.send('/input/Voice', 0),
+	'keyUpLookLeft': 		() => oscClient.send('/input/LookLeft', 0),
+	'keyUpLookRight': 	() => oscClient.send('/input/LookRight', 0),
+}
+
 // VRChat OSC
 const { Client, Server } = require('node-osc')
 const oscClient = new Client('127.0.0.1', 9000)
 const oscServer = new Server(9001, '127.0.0.1', () => {
 	console.log(chalk.cyan(`[${new Date().toLocaleTimeString()}]`), chalk.yellow('OSC Server online at 9000'))
-})
-
-oscServer.on('message', (msg) => {
 })
 
 // Express for WebSockets
@@ -63,34 +85,8 @@ function processMessage(message) {
 }
 
 function processInput(message) {
-	const inputMap = {
-		'keyDownForward': () => oscClient.send('/input/MoveForward', true),
-		'keyDownBackward': () => oscClient.send('/input/MoveBackward', true),
-		'keyDownLeft': () => oscClient.send('/input/MoveLeft', true),
-		'keyDownRight': () => oscClient.send('/input/MoveRight', true),
-		'keyDownSprint': () => oscClient.send('/input/Run', true),
-		'keyDownJump': () => oscClient.send('/input/Jump', 1),
-		'keyDownCrouch': () => oscClient.send('/input/GrabAxisRight', 1),
-		'keyDownProne': () => oscClient.send('/avatar/parameters/Upright', 0.35),
-		'keyDownVoice': () => oscClient.send('/input/Voice', 1),
-		'keyDownLookLeft': () => oscClient.send('/input/LookLeft', 1),
-		'keyDownLookRight': () => oscClient.send('/input/LookRight', 1),
-		'keyUpForward': () => oscClient.send('/input/MoveForward', false),
-		'keyUpBackward': () => oscClient.send('/input/MoveBackward', false),
-		'keyUpLeft': () => oscClient.send('/input/MoveLeft', false),
-		'keyUpRight': () => oscClient.send('/input/MoveRight', false),
-		'keyUpSprint': () => oscClient.send('/input/Run', false),
-		'keyUpJump': () => oscClient.send('/input/Jump', 0),
-		'keyUpCrouch': () => oscClient.send('/avatar/parameters/Go/Horizon', false),
-		'keyUpProne': () => oscClient.send('/avatar/parameters/Upright', 1),
-		'keyUpVoice': () => oscClient.send('/input/Voice', 0),
-		'keyUpLookLeft': () => oscClient.send('/input/LookLeft', 0),
-		'keyUpLookRight': () => oscClient.send('/input/LookRight', 0),
-	}
-
-	if (inputMap[message]) {
+	if (inputMap[message])
 		inputMap[message]()
-	}
 	else
 		return;
 
