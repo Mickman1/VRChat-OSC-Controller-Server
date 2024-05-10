2b9e5319de2a7f177ac5dd532130ece5c1160c67
Mickman <70730433+Mickman1@users.noreply.github.com>
2024-05-09 22:51:43 -0400

    switch statement -> input map
    - slightly more efficient
    - much less of an eyesore

--- websocket_server.js
+++ websocket_server.js
@@ -14,7 +14,6 @@ const oscServer = new Server(9001, '127.0.0.1', () => {
 })
 
 oscServer.on('message', (msg) => {
-	console.log(chalk.cyan(`${new Date().toLocaleTimeString()}:`), chalk.grey(msg))
 })
 
 // Express for WebSockets
@@ -64,56 +63,36 @@ function processMessage(message) {
 }
 
 function processInput(message) {
-	switch (message) {
-		case 'keyDownForward':
-			oscClient.send('/input/MoveForward', true)
-			break
-		case 'keyDownBackward':
-			oscClient.send('/input/MoveBackward', true)
-			break
-		case 'keyDownLeft':
-			oscClient.send('/input/MoveLeft', true)
-			break
-		case 'keyDownRight':
-			oscClient.send('/input/MoveRight', true)
-			break
-		case 'keyDownSprint':
-			oscClient.send('/input/Run', true)
-			break
-		case 'keyDownJump':
-			oscClient.send('/input/Jump', 1)
-			break
-		case 'keyDownCrouch':
-			oscClient.send('/avatar/parameters/Upright', 0.6)
-			break
-		case 'keyDownProne':
-			oscClient.send('/avatar/parameters/Upright', 0.35)
-			break
-		case 'keyUpForward':
-			oscClient.send('/input/MoveForward', false)
-			break
-		case 'keyUpBackward':
-			oscClient.send('/input/MoveBackward', false)
-			break
-		case 'keyUpLeft':
-			oscClient.send('/input/MoveLeft', false)
-			break
-		case 'keyUpRight':
-			oscClient.send('/input/MoveRight', false)
-			break
-		case 'keyUpSprint':
-			oscClient.send('/input/Run', false)
-			break
-		case 'keyUpJump':
-			oscClient.send('/input/Jump', 0)
-			break
-		case 'keyUpCrouch':
-			oscClient.send('/avatar/parameters/Upright', 1)
-			break
-		case 'keyUpProne':
-			oscClient.send('/avatar/parameters/Upright', 1)
-			break
+	const inputMap = {
+		'keyDownForward': () => oscClient.send('/input/MoveForward', true),
+		'keyDownBackward': () => oscClient.send('/input/MoveBackward', true),
+		'keyDownLeft': () => oscClient.send('/input/MoveLeft', true),
+		'keyDownRight': () => oscClient.send('/input/MoveRight', true),
+		'keyDownSprint': () => oscClient.send('/input/Run', true),
+		'keyDownJump': () => oscClient.send('/input/Jump', 1),
+		'keyDownCrouch': () => oscClient.send('/input/GrabAxisRight', 1),
+		'keyDownProne': () => oscClient.send('/avatar/parameters/Upright', 0.35),
+		'keyDownVoice': () => oscClient.send('/input/Voice', 1),
+		'keyDownLookLeft': () => oscClient.send('/input/LookLeft', 1),
+		'keyDownLookRight': () => oscClient.send('/input/LookRight', 1),
+		'keyUpForward': () => oscClient.send('/input/MoveForward', false),
+		'keyUpBackward': () => oscClient.send('/input/MoveBackward', false),
+		'keyUpLeft': () => oscClient.send('/input/MoveLeft', false),
+		'keyUpRight': () => oscClient.send('/input/MoveRight', false),
+		'keyUpSprint': () => oscClient.send('/input/Run', false),
+		'keyUpJump': () => oscClient.send('/input/Jump', 0),
+		'keyUpCrouch': () => oscClient.send('/avatar/parameters/Go/Horizon', false),
+		'keyUpProne': () => oscClient.send('/avatar/parameters/Upright', 1),
+		'keyUpVoice': () => oscClient.send('/input/Voice', 0),
+		'keyUpLookLeft': () => oscClient.send('/input/LookLeft', 0),
+		'keyUpLookRight': () => oscClient.send('/input/LookRight', 0),
 	}
+
+	if (inputMap[message]) {
+		inputMap[message]()
+	}
+	else
+		return;
 
 	console.log(chalk`{cyan [${new Date().toLocaleTimeString()}]} {white 🕹️: ${message}}`)
 }
